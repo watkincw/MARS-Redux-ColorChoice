a1016702b21dbfe566bb9013f98b68abfdf5ae3b
Colton Watkins <watkincw@gmail.com>
2022-03-24 12:49:16 -0500

    created store and used 'Provider' over 'React.StrictMode'

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import chosenColorReducer from './Reducers/ChooseColorReducers';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(chosenColorReducer);
+console.log('Store Created!');
+
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById('root')
 );
 
