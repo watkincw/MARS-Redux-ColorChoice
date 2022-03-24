a1d702dabb8c741d7b6f1b5c3d3b2f1db652a26d
Colton Watkins <watkincw@gmail.com>
2022-03-24 12:50:07 -0500

    finished assignment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { red, blue, green, purple } from './Actions/ChooseColorActions';
 
 function App() {
+	const color = useSelector(state=>state);
+	const dispatch = useDispatch();
 	return (
 		<div className="App">
+			<h1>Which color do you prefer between these choices?</h1>
+			<button type='radio' onClick={() => dispatch(red())}>Red</button>
+			<button type='radio' onClick={() => dispatch(blue())}>Blue</button>
+			<button type='radio' onClick={() => dispatch(green())}>Green</button>
+			<button type='radio' onClick={() => dispatch(purple())}>Purple</button>
+			<br />
+			<h3>
+				Selected Color: {color}
+			</h3>
 		</div>
 	);
 }
