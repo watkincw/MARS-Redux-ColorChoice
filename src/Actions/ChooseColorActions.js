a82142a4da36ac0d01852c5552d3eff2c1825463
Colton Watkins <watkincw@gmail.com>
2022-03-24 12:47:29 -0500

    created Actions

---
+++ src/Actions/ChooseColorActions.js
@@ -0,0 +1,23 @@
+export const red = () => {
+    return {
+        type: 'RED'
+    };
+}
+
+export const blue = () => {
+    return {
+        type: 'BLUE'
+    };
+}
+
+export const green = () => {
+    return {
+        type: 'GREEN'
+    };
+}
+
+export const purple = () => {
+    return {
+        type: 'PURPLE'
+    };
+}
